77fe9e9afa2b56e00ac1a01e4105dddcfbc31277
naved255 <navedahmad2314lko@gamil.com>
2026-07-27 23:37:36 +0530

    fixed

--- gath_assign_backend/index.js
+++ gath_assign_backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["https://gath-assignment.vercel.app", "http://localhost:5173"], 
     credentials: true, 
   })
 );
